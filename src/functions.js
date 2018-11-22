b5adaff2a53a27b74a0af8e3e569cbe84bf7bf04
jurriaan <jurriaan@iksi.eu>
2018-11-22 11:34:50 +0100

    Update functions.js

--- src/functions.js
+++ src/functions.js
@@ -5,3 +5,23 @@ function qs (selectors, context) {
 function qsa (selectors, context) {
   return (context || document).querySelectorAll(selectors)
 }
+
+function raf (callback, interval) {
+  interval = interval || 1000
+  var start, delta
+
+  function tick (timestamp) {
+    start = start || timestamp
+    delta = timestamp - start
+
+    if (delta > interval || start === timestamp) {
+      start = timestamp - delta % interval
+
+      callback()
+    }
+
+    window.requestAnimationFrame(tick)
+  }
+
+  window.requestAnimationFrame(tick)
+}
